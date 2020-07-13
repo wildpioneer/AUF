1aaf8297042700f80d42ce420c816416f35c94a6
wildpioneer <atrostyanko@gmail.com>
2020-07-13 16:13:27 +0300

    Added several new tests for API

---
+++ src/main/java/org/techmeskills/aqa5/auf/baseEntity/BaseApiTest.java
@@ -0,0 +1,27 @@
+package org.techmeskills.aqa5.auf.baseEntity;
+
+import org.techmeskills.aqa5.auf.core.BrowsersService;
+import org.techmeskills.aqa5.auf.core.ReadProperties;
+import org.techmeskills.aqa5.auf.utils.Listener;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+
+@Listeners(Listener.class)
+public abstract class BaseApiTest {
+    public BrowsersService browsersService;
+    public ReadProperties properties;
+
+    @BeforeClass
+    public void openPage() {
+        browsersService = new BrowsersService();
+        properties = new ReadProperties();
+        browsersService.getDriver().get(properties.getURL());
+    }
+
+    @AfterClass
+    public void closePage() {
+        browsersService.getDriver().quit();
+        browsersService = null;
+    }
+}
