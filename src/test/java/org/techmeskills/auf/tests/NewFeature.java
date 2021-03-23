1b12e6a453aab099843f073328caeb5c0fe90acc
Alexandr Trostyanko <Aleksandr.Trostyanko@itechart-group.com>
2021-03-23 20:21:59 +0300

    Все сделано

---
+++ src/test/java/org/techmeskills/auf/tests/NewFeature.java
@@ -0,0 +1,11 @@
+package org.techmeskills.auf.tests;
+
+import org.testng.annotations.Test;
+
+public class NewFeature {
+
+    @Test
+    public void ThirdTest() {
+        //sjdfhskdfhskjdh
+    }
+}
